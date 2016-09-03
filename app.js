1b637ee317c1592f78575170d6fb522755f5dae6
faulke <faulkev@gmail.com>
2016-09-02 23:10:01 -0600

    Fix request with lat long

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const async = require('async');
+const request = require('request');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const apiKey = '881b5955fcd17cbec3fe94131e417545';
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -8,21 +11,31 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
-app.get('/home', (req, res) => {
+app.get('/home', (req, res, next) => {
   res.render('index.pug');
+  next();
 });
 
-app.post('/action', require('./middleware.js'));
+app.get('/weather/:lat/:long', (req, res) => {
+  console.log(req.params);
+  const lat = req.params.lat;
+  const long = req.params.long;
+  const urls = [`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${apiKey}`,
+                `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&APPID=${apiKey}`];
 
-app.get('/weather', (req, res) => {
-  setTimeout(() => {
-    const weather = req.app.settings.weatherData;
-    const forecast = req.app.settings.forecast;
-    console.log(weather)
-    res.render('template', { title: 'Hey', weather: weather, forecast: forecast });
-  }, 2000);
+  async.map(urls, (url, callback) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        console.error(err);
+      } else {
+        callback(null, JSON.parse(body));
+      }
+    });
+  }, (err, results) => {
+    res.render('template', { title: 'Weather', weather: results[0], forecast: results[1] })
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000');
 });
--- public/js/locate.js
+++ public/js/locate.js
@@ -3,13 +3,7 @@ $(document).ready(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
       const long = position.coords.longitude;
-  //    $.post('http://localhost:3000/action', { latitude: lat, longitude: long });
-      $.ajax({
-        type: 'POST',
-        url: 'http://localhost:3000/action',
-        data: { latitude: lat, longitude: long },
-        success: window.location = '/weather',
-      });
+      window.location = `/weather/${lat}/${long}`;
     });
   }
 });
