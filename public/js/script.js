a855593eb8ce0b1ed765c44c372eb22d2edba649
faulke <faulkev@gmail.com>
2016-09-07 20:05:05 -0600

    Update js structure

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,57 @@
-$(document).ready(() => {
-  let i;
-  const d = new Date();
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  $('#faren').prop('disabled', true);
-  $('#faren').addClass('button-active');
+// https://codepen.io/FreeCodeCamp/pen/KzXQgy
 
-  function setTime(time, suntime) {
+const myApp = {
+  initialize: () => {
+    myApp.display.setButton();
+    myApp.display.setBackground();
+    myApp.display.setDays();
+    myApp.display.setTimes();
+    myApp.display.convertValues();
+  }
+}
+
+myApp.display = {
+  setButton: () => {
+    $('#faren').prop('disabled', true);
+    $('#faren').addClass('button-active');
+  },
+  setDays: () => {
+    const titles = $('.forecastDay');
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const d = new Date();
+    let i;
+
+    for (i = 0; i < titles.length; i++) {
+      d.setDate(d.getDate() + 1);
+      $(titles[i]).text(days[d.getDay()]);
+    }
+  },
+  setTimes: () => {
+    const oldSunrise = $('#sunrise').text();
+    const oldSunset = $('#sunset').text();
+
+    $('#sunrise').html(myApp.helpers.setTime(oldSunrise, 'sunrise'));
+    $('#sunset').html(myApp.helpers.setTime(oldSunset, 'sunset'));
+  },
+  convertValues: () => {
+    const pressure = $('#pressure').text();
+    $('#pressure').html(myApp.helpers.convertPressure(pressure));
+
+    const temps = $('.temp');
+    let i;
+    for (i = 0; i < temps.length; i++) {
+      const temp = $(temps[i]).html();
+      const newTemp = myApp.helpers.kelvinToFaren(temp);
+      $(temps[i]).text(`${newTemp} ${String.fromCharCode(176)}F`);
+    }
+  },
+  setBackground: () => {
+
+  }
+}
+
+myApp.helpers = {
+  setTime: (time, suntime) => {
     const dateTime = parseInt(time, 10);
     const newDate = new Date(dateTime * 1000);
     const hours = newDate.getHours() > 12 ? (newDate.getHours() - 12) : newDate.getHours();
@@ -15,66 +61,54 @@ $(document).ready(() => {
     } else {
       return `${hours}:${minutes} PM`;
     }
-  }
-
-// https://codepen.io/FreeCodeCamp/pen/KzXQgy
-
-  const oldSunrise = $('#sunrise').text();
-  $('#sunrise').html(setTime(oldSunrise, 'sunrise'));
-
-  const oldSunset = $('#sunset').text();
-  $('#sunset').html(setTime(oldSunset, 'sunset'));
-
-  const pressure = $('#pressure').text();
-  $('#pressure').html((parseInt(pressure, 10) / 25.4).toFixed(1));
-
-  function kelvinToFaren(temp) {
-    const dat = parseFloat(temp);
-    return Math.round((dat - 273.15) * 1.8000 + 32.00);
-  }
-
-  function farenToCels(temp) {
+  },
+  convertPressure: (pressure) => {
+    return (parseInt(pressure, 10) / 25.4).toFixed(1);
+  },
+  kelvinToFaren: (temp) => {
+    return Math.round((parseFloat(temp) * (9/5)) - 459.67);
+  },
+  farenToCels: (temp) => {
     return Math.round((temp - 32) * (5 / 9));
+  },
+  celsToFaren: (temp) => {
+    return Math.round((temp * 1.8) + 32);
   }
+}
 
-  function CelsToFaren(temp) {
-    return Math.round((temp * 1.8) + 32)
-  }
+myApp.actions = {
+  selectButton: (id) => {
+    const temps = $('.temp');
 
-  const titles = $('.forecastDay');
-  const temps = $('.temp');
-
-  for (i = 0; i < titles.length; i++) {
-    d.setDate(d.getDate() + 1);
-    $(titles[i]).text(days[d.getDay()]);
-  }
-
-  for (i = 0; i < temps.length; i++) {
-    const temp = $(temps[i]).html();
-    $(temps[i]).text(`${kelvinToFaren(temp)} ${String.fromCharCode(176)}F`);
-  }
-
-  $('#cels').on('click', () => {
     for (i = 0; i < temps.length; i++) {
       const dig = $(temps[i]).html();
       const temp = dig.match(/\d/g).join('');
-      $(temps[i]).text(`${farenToCels(temp)} ${String.fromCharCode(176)}C`);
+      const newTemp = id === 'cels' ? myApp.helpers.farenToCels(temp) : myApp.helpers.celsToFaren(temp);
+      $(temps[i]).text(`${newTemp} ${String.fromCharCode(176)}C`);
     }
-    $('#cels').addClass('button-active')
-    $('#faren').removeClass('button-active')
-    $('#cels').prop('disabled', true);
-    $('#faren').prop('disabled', false);
-  });
 
-  $('#faren').on('click', () => {
-    for (i = 0; i < temps.length; i++) {
-      const dig = $(temps[i]).html();
-      const temp = dig.match(/\d/g).join('');
-      $(temps[i]).text(`${CelsToFaren(temp)} ${String.fromCharCode(176)}F`);
-    }
-    $('#faren').prop('disabled', true);
-    $('#faren').addClass('button-active')
-    $('#cels').prop('disabled', false);
-    $('#cels').removeClass('button-active')
-  })
+    if (id === 'cels') {
+      $('#cels').addClass('button-active')
+      $('#faren').removeClass('button-active')
+      $('#cels').prop('disabled', true);
+      $('#faren').prop('disabled', false);
+    } else {
+        $('#faren').prop('disabled', true);
+        $('#faren').addClass('button-active')
+        $('#cels').prop('disabled', false);
+        $('#cels').removeClass('button-active')
+      }
+  },
+  search: () => {
+    const input = $('form input').val();
+    $.post('http://localhost:3000/search', {data: input}, (response, status) => {
+      const lat = response.results[0].geometry.location.lat;
+      const long = response.results[0].geometry.location.lng;
+      window.location = `/weather/${lat}/${long}`;
+    });
+  }
+}
+
+$(document).ready(() => {
+  myApp.initialize();
 });
