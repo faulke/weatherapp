ca16b39b37c534b97385b4f54b0cc823848ed52c
faulke <faulkev@gmail.com>
2016-11-08 07:54:19 -0800

    Allow dynamic port for heroku

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get('/weather/:lat/:long', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000');
 });
