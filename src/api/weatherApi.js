f85f4f7a2c167796f74677faa9f06c3d6ba12899
faulke <faulkev@gmail.com>
2016-12-19 23:20:22 -0700

    Update weather api
    - get weather by lat/long
    - search by city, st or address

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,8 +1,8 @@
 import async from 'async';
 import request from 'request';
 
-const weatherKey = process.env.WEATHER_KEY;
-console.log(weatherKey);
+const weatherKey = 'weather_key';
+const googleKey = 'google_key';
 
 class WeatherApi {
   static getWeather(lat, long, cb) {
@@ -22,10 +22,26 @@ class WeatherApi {
         }
       });
     }, (err, results) => {
-      if (results[0].cod === 200) {
+      if (!err && results[0].cod === 200) {
         cb(null, results);
       } else {
         cb(err);
+      }
+    });
+  }
+  
+  static searchWeather(search, cb) {
+    const self = this;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${googleKey}`;
+    request.get({
+      url,
+      withCredentials: false,
+    }, (err, result, body) => {
+      if (err) {
+        cb(err);
+      } else {
+        const data = JSON.parse(body);
+        cb(null, data);
       }
     });
   }
