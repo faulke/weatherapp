f2931c3048f20e4fd366b29922d34b063a4feeb1
faulke <faulkev@gmail.com>
2017-01-07 03:06:47 -0700

    Add search container

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,45 +1,21 @@
-import React from 'react';
-import WeatherApi from '../../api/weatherApi';
+import React, { Component } from 'react';
+
 // TODO: implement google autocomplete for addresses
-class Search extends React.Component {
+class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    const self = this;
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        if (position) {
-          const lat = position.coords.latitude;
-          const long = position.coords.longitude;
-          WeatherApi.searchWeather('latlng', `${lat},${long}`, (err, data) => {
-            console.log(data.results);
-            for (let i = 0; i < data.results.length; i += 1) {
-              if (data.results[i].types.indexOf('locality') > -1) {
-                self.setState({ value: data.results[i].formatted_address });
-                break;
-              }
-            }
-          });
-           // if location, suggest location for weather
-           // browserHistory.push(`/${lat}/${long}`);
-        }
-      });
-    }
-  }
-
   handleSubmit(evt) {
-    this.props.onSubmit(evt, this.state);
+    this.props.onSubmit(evt);
     evt.preventDefault();
   }
 
   handleChange(evt) {
-    this.setState({ value: evt.target.value });
+    this.props.onChange(evt);
   }
 
   render() {
@@ -48,7 +24,7 @@ class Search extends React.Component {
         <span className="glyphicon glyphicon-cloud" />
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} placeholder="City, ST" size="50" />
+            <input type="text" className="form-control" value={this.props.value} onChange={this.handleChange} placeholder="City, ST" size="50" />
             <span className="help-block" />
             <button type="submit" className="btn btn-primary" data-loading-text="Searching...">
               Search {'\u00A0'}
@@ -60,9 +36,5 @@ class Search extends React.Component {
     );
   }
 }
-
-Search.propTypes = {
-  onSubmit: React.PropTypes.func.isRequired,
-};
 
 export default Search;
