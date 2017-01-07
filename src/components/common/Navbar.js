e6dd97b1768adfae5034005041b5d619204dddd9
faulke <faulkev@gmail.com>
2017-01-06 23:22:17 -0700

    Update navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, NavItem, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Link, Nav, Navbar, NavItem, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 class navbarInstance extends React.Component {
   constructor(props) {
@@ -19,21 +19,23 @@ class navbarInstance extends React.Component {
     this.setState({ value: evt.target.value });
   }
 
+// TODO: add linkcontainer for Home
   render() {
     return (
       <Navbar inverse collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
           <Navbar.Toggle />
+          <Navbar.Brand>
+            Orchard Homes (icon) temp
+          </Navbar.Brand>
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
+          <Nav pullLeft>
+            <NavItem>
+              Home
+            </NavItem>
           </Nav>
-          <form className="navbar-form" onSubmit={this.handleSubmit}>
+          <form className="navbar-form navbar-right" onSubmit={this.handleSubmit}>
             <div className="form-group">
               <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} placeholder="City, ST" size="50" />
               <span className="help-block" />
