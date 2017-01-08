2eeccac9b92d2eb3e74f0ad7e73bee24824c368c
Evan Faulk <faulkev@gmail.com>
2017-01-08 16:15:09 -0700

    Link weather table items to weather page (#29)
    * Make weather table items clickable
    
    - Pass lat/long through click handler
    - Push lat/long to history and render weather
    - Make weather table a stateless functional component
    
    * Add comment for later

--- src/components/home/WeatherTable.js
+++ src/components/home/WeatherTable.js
@@ -3,18 +3,19 @@ import { Table } from 'react-bootstrap';
 import WeatherApi from '../../api/weatherApi';
 
 // TODO: implement google autocomplete for addresses
-class WeatherTable extends React.Component {
-  render() {
-    return (
-      <Table bordered condensed hover>
-        <tbody>
-          {
-            this.props.weather.map(x => <tr key={x.id}><td>{x.name}</td></tr>)
-          }
-        </tbody>
-      </Table>
-    );
-  }
-}
+const WeatherTable = ({ weather, onClick }) => {
+  return (
+    <Table bordered condensed hover>
+      <tbody>
+        {
+          weather.map((x) => {
+            const url = `${x.coord.lat}/${x.coord.lon}`;
+            return <tr key={x.id}><td data-href={url} onClick={onClick}>{x.name}</td></tr>;
+          })
+        }
+      </tbody>
+    </Table>
+  );
+};
 
 export default WeatherTable;
