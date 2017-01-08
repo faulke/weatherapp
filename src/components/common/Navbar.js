bd3d6b543b33c2811ae1d80da4e7b25c3400e45d
faulke <faulkev@gmail.com>
2017-01-08 12:47:24 -0700

    Updat navbar to react bootstrap nav

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { Link, Nav, Navbar, NavItem, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Link, Nav, Navbar, NavItem } from 'react-bootstrap';
+import SearchContainer from '../../containers/SearchContainer';
 
 class navbarInstance extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(evt) {
-    this.props.onSubmit(evt, this.state);
-    evt.preventDefault();
-  }
-
-  handleChange(evt) {
-    this.setState({ value: evt.target.value });
-  }
 
 // TODO: add linkcontainer for Home
   render() {
@@ -35,16 +20,7 @@ class navbarInstance extends React.Component {
               Home
             </NavItem>
           </Nav>
-          <form className="navbar-form navbar-right" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} placeholder="City, ST" size="50" />
-              <span className="help-block" />
-              <button type="submit" className="btn btn-primary" data-loading-text="Searching...">
-                Search {'\u00A0'}
-                <span className="glyphicon glyphicon-search" />
-              </button>
-            </div>
-          </form>
+          <SearchContainer />
         </Navbar.Collapse>
       </Navbar>
     );
