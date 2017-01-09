40c75165b70c9d5c3467dc75ca8731ad7f98a8f4
faulke <faulkev@gmail.com>
2017-01-08 23:31:21 -0700

    Fix inline search form

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 // TODO: implement google autocomplete for addresses
 const Search = (props) => {
   return (
-    <Form inline className="navbar-form navbar-right" onSubmit={props.onSubmit}>
+    <Form inline={props.inline} className={props.place} onSubmit={props.onSubmit}>
       <FormGroup>
-        <FormControl type="text" placeholder="City, ST" value={props.value} onChange={props.onChange} size={props.size} />
+        <FormControl 
+          type="text" 
+          placeholder="City, ST" 
+          value={props.value} 
+          onChange={props.onChange} 
+          size={props.size} 
+        />
+        <Button type="submit" bsStyle="primary">
+          Search {'\u00A0'}
+          <span className="glyphicon glyphicon-search" />
+        </Button>
       </FormGroup>
-      {' '}
-      <Button type="submit" bsStyle="primary">
-        Search {'\u00A0'}
-        <span className="glyphicon glyphicon-search" />
-      </Button>
     </Form>
   );
 };
