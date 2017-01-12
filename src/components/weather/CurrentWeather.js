f1934091493e8580f276e95051057e32868391fd
faulke <faulkev@gmail.com>
2017-01-11 21:41:05 -0700

    Add weather description

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -9,6 +9,7 @@ const CurrentWeather = (props) => {
   const temp = Math.round(current.main.temp);
   const wind = Math.round(current.wind.speed);
   const icon = current.weather[0].id;
+  const description = current.weather[0].description;
   const iconClass = `wi wi-owm-${icon}`;
 
   return (
@@ -22,7 +23,7 @@ const CurrentWeather = (props) => {
         <Row className="text-center">
           <Col sm={2} smOffset={3}>
             <h1><i className={iconClass} /></h1>
-            <h3>Description</h3>
+            <h3>{description}</h3>
           </Col>
           <Col sm={2}>
             <h1>{temp}&deg;F</h1>
