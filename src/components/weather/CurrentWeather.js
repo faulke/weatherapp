cdc09d44a970c35a63deea36e21a244f226d3086
faulke <faulkev@gmail.com>
2017-01-11 23:04:59 -0700

    Remove unnecessary div

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -1,45 +1,40 @@
-import React, { Component, PropTypes } from 'react';
-import { Link, browserHistory } from 'react-router';
+import React, { PropTypes } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-import Navbar from '../common/Navbar';
-import WeatherApi from '../../api/weatherApi';
 
 const CurrentWeather = (props) => {
   const current = props.current;
-  const temp = Math.round(current.main.temp);
   const wind = Math.round(current.wind.speed);
-  const icon = current.weather[0].id;
   const description = current.weather[0].description;
-  const iconClass = `wi wi-owm-${icon}`;
+  const iconClass = `wi wi-owm-${props.icon}`;
 
   return (
-    <div>
-      <Grid>
-        <Row className="show-grid">
-          <Col xs={6} xsOffset={3} className="text-center">
-            <h1>{current.name}</h1>
-          </Col>
-        </Row>
-        <Row className="text-center">
-          <Col sm={2} smOffset={3}>
-            <h1><i className={iconClass} /></h1>
-            <h3>{description}</h3>
-          </Col>
-          <Col sm={2}>
-            <h1>{temp}&deg;F</h1>
-          </Col>
-          <Col sm={2}>
-            <h3>Wind</h3>
-            <h4>{wind} mph</h4> 
-          </Col>
-        </Row>
-      </Grid>
-    </div>
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={6} xsOffset={3} className="text-center">
+          <h1>{current.name}</h1>
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col sm={2} smOffset={3}>
+          <h1><i className={iconClass} /></h1>
+          <h3>{description}</h3>
+        </Col>
+        <Col sm={2}>
+          <h1>{props.temp}&deg;F</h1>
+        </Col>
+        <Col sm={2}>
+          <h3>Wind</h3>
+          <h4>{wind} mph</h4> 
+        </Col>
+      </Row>
+    </Grid>
   );
 };
 
 CurrentWeather.propTypes = {
   current: React.PropTypes.object.isRequired,
+  icon: React.PropTypes.number.isRequired,
+  temp: React.PropTypes.number.isRequired,
 };
 
 export default CurrentWeather;
