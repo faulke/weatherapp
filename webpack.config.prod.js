21acaa820f6856d27c2b70044b149e56eb419745
faulke <faulkev@gmail.com>
2017-01-16 22:11:23 -0700

    Add webpack prod config

---
+++ webpack.config.prod.js
@@ -0,0 +1,61 @@
+import webpack from 'webpack';
+import path from 'path';
+import dotenv from 'dotenv';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production')
+};
+
+dotenv.config();
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: './src/index',
+  target: 'web',
+  output: {
+    path: `${__dirname}/dist`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'WEATHER_KEY': `"${process.env.WEATHER_KEY}"`,
+        'GOOGLE_KEY': `"${process.env.GOOGLE_KEY}"`,
+      },
+    }),
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+      { test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap") },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    ],
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+  },
+};
