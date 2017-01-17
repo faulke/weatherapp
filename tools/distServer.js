c35724344d598eb86386b5064e5d3103a2fcca71
faulke <faulkev@gmail.com>
2017-01-16 22:11:33 -0700

    Add prod server

---
+++ tools/distServer.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(favicon(path.join(__dirname, '../src/favicon.ico')));
+
+app.use(express.static('dist'));
+
+app.get('/:lat/:long', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port);
