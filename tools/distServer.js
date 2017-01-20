bb7cfaf9483347ed337e60ef5f6ad4fb1d6cbce2
faulke <faulkev@gmail.com>
2017-01-19 23:58:24 -0700

    Update dist server for proxy

--- tools/distServer.js
+++ tools/distServer.js
@@ -2,6 +2,10 @@
 import express from 'express';
 import path from 'path';
 import favicon from 'serve-favicon';
+import api from './routes/api';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,16 +14,10 @@ app.use(favicon(path.join(__dirname, '../src/favicon.ico')));
 
 app.use(express.static('dist'));
 
-app.get('/:lat/:long', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
+app.use('/api', api);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port);
