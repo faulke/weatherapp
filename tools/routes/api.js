87ce85098c96c88c168c0ec798dcbebe85fc28ca
faulke <faulkev@gmail.com>
2017-01-19 23:23:13 -0700

    Use express router for api routes

---
+++ tools/routes/api.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import request from 'request';
+import crypto from 'crypto';
+import compare from 'secure-compare';
+
+const router = express.Router();
+
+const auth = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send('401 Not authorized.');
+  }
+  const basic = req.headers.authorization.split(' ')[1];
+  const clientKey = crypto.createHmac('sha1', process.env.SECRET_KEY).update(basic).digest('hex');
+  const authorized = compare(clientKey, process.env.API_KEY);
+  if (!authorized) {
+    return res.status(401).send('401 Not authorized.');
+  }
+  return next();
+};
+
+router.get('/geocode', auth, (req, res) => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?${req.query.type}=${req.query.search}&key=${process.env.GOOGLE_KEY}`;
+  return req.pipe(request(url)).pipe(res);
+});
+
+router.get('/weather', auth, (req, res) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&units=imperial&APPID=${process.env.WEATHER_KEY}`;
+  return req.pipe(request(url)).pipe(res);
+});
+
+router.get('/forecast', auth, (req, res) => {
+  const url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&cnt=5&units=imperial&APPID=${process.env.WEATHER_KEY}`;
+  return req.pipe(request(url)).pipe(res);
+});
+
+module.exports = router;
--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,6 +6,7 @@ import webpack from 'webpack';
 import path from 'path';
 import open from 'open';
 import favicon from 'serve-favicon';
+import api from './routes/api';
 import config from '../webpack.config.dev';
 
 const port = process.env.PORT || 3000;
@@ -14,27 +15,14 @@ const compiler = webpack(config);
 
 app.use(favicon(path.join(__dirname, '../src/favicon.ico')));
 
-app.use('/api/geocode', (req, res) => {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?${req.query.type}=${req.query.search}&key=${process.env.GOOGLE_KEY}`;
-  req.pipe(request(url)).pipe(res);
-});
-
-app.use('/api/weather/', (req, res) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&units=imperial&APPID=${process.env.WEATHER_KEY}`;
-  req.pipe(request(url)).pipe(res);
-});
-
-app.use('/api/forecast/', (req, res) => {
-  const url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&cnt=5&units=imperial&APPID=${process.env.WEATHER_KEY}`;
-  req.pipe(request(url)).pipe(res);
-});
-
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
+
+app.use('/api', api);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
