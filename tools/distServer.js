2631706c3dde3e3bf120732ee2a8543d3edebbb2
faulke <faulkev@gmail.com>
2017-01-29 22:19:30 -0700

    Fix root directory error

--- tools/distServer.js
+++ tools/distServer.js
@@ -17,7 +17,7 @@ app.use(express.static('dist'));
 app.use('/api', api);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port);
