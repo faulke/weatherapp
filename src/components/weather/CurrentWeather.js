adf955da115eb6215fad67dc6700904358012872
faulke <faulkev@gmail.com>
2017-01-29 17:49:20 -0700

    Use props explicitly

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -1,11 +1,10 @@
 import React, { PropTypes } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-const CurrentWeather = (props) => {
-  const current = props.current;
+const CurrentWeather = ({ current, icon, temp }) => {
   const wind = Math.round(current.wind.speed);
   const description = current.weather[0].description;
-  const iconClass = `wi wi-owm-${props.icon}`;
+  const iconClass = `wi wi-owm-${icon}`;
 
   return (
     <Grid>
@@ -20,7 +19,7 @@ const CurrentWeather = (props) => {
           <h4>{description}</h4>
         </Col>
         <Col sm={2}>
-          <h1 className="main-temp">{props.temp}&deg;F</h1>
+          <h1 className="main-temp">{temp}&deg;F</h1>
           <span><h5>Wind</h5><h6>{wind} mph</h6></span>
         </Col>
       </Row>
