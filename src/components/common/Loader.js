3602700b1fadece94f4da817694895df130fc117
faulke <faulkev@gmail.com>
2017-02-01 18:36:04 -0700

    Create loader class
    Conditionally renders based on isFetching

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const Loader = () =>
-  <div className="loader" />;
+class Loader extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.loading ? (
+          <div className="loader" />  
+        ) : (
+          this.props.children
+        )}
+      </div>
+    );
+  }
+}
+
+Loader.propTypes = {
+  loading: React.PropTypes.bool.isRequired,
+  children: React.PropTypes.object.isRequired,
+};
 
 export default Loader;
