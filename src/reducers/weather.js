03621545a286f890796223a8b731608a969045ca
faulke <faulkev@gmail.com>
2017-02-02 21:09:01 -0700

    Add spread operator and weather multiple

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -3,6 +3,7 @@ import {
   UPDATE_SEARCH,
   REQUEST_WEATHER, 
   RECEIVE_WEATHER,
+  REQUEST_WEATHER_MULTIPLE,
   RECEIVE_WEATHER_MULTIPLE, 
   SET_LOCATION, 
   SET_TABLE,
@@ -23,47 +24,45 @@ export const initialState = {
   forecast: null,
   tableWeather: null,
   isFetching: false,
+  celsius: false,  
 };
 
 export const weather = (state = initialState, action) => {
   const { type } = action;
   switch (type) {
     case UPDATE_SEARCH:
-      return Object.assign({}, state, {
-        search: action.input,
-      });
+      return { ...state, search: action.input };
     case SEARCH_LOCATION:
-      return Object.assign({}, state, {
-        search: action.search,
-      });
+      return { ...state, search: action.search };
     case REQUEST_WEATHER:
-      return Object.assign({}, state, {
-        isFetching: true,
-      });
+      return { ...state, isFetching: true };
     case RECEIVE_WEATHER:
-      return Object.assign({}, state, {
+      return { ...state,
         now: action.weather[0],
         forecast: action.weather[1].list,
         isFetching: false,
-      });
+      };
+    case REQUEST_WEATHER_MULTIPLE:
+      return { ...state, isFetching: true };
     case RECEIVE_WEATHER_MULTIPLE:
-      return Object.assign({}, state, {
+      return { ...state,
         tableWeather: action.tableWeather,
-      });
+        isFetching: false,
+      };
     case SET_LOCATION:
-      return Object.assign({}, state, {
+      return { ...state,
         location: {
           lat: action.lat,
           long: action.long,
         },
-      });
+      };
     case SET_TABLE:
-      return Object.assign({}, state, {
+      return { ...state,
         table: [{
           lat: action.lat,
           long: action.long,
         }, state.table[1], state.table[2]],
-      });
+      };
     default:
       return state;
   }
