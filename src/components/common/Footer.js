e046db7edc17c3c45c8845f2930c52955425a247
faulke <faulkev@gmail.com>
2017-02-12 13:36:29 -0700

    Add styles to footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navbar, Button } from 'react-bootstrap';
+import styles from './footer.less';
 
 const Footer = ({ onClick }) =>
-  <Navbar inverse fluid fixedBottom className="footer">
-    <Navbar.Header>
+  <Navbar inverse fluid fixedBottom>
+    <Navbar.Header className={styles.buttons}>
       <Button id="fahr" bsSize="small" onClick={onClick}>&deg;F</Button>
       <Button id="cels" bsSize="small" onClick={onClick}>&deg;C</Button>
     </Navbar.Header>
