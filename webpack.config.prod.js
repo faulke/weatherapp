ed499b3809c998bb2afd98393a687184087a57ca
faulke <faulkev@gmail.com>
2017-02-12 12:42:51 -0700

    Update webpack prod config
    - Hash filenames to help with caching
    - Include less-loader
    - Use html-webpack-plugin to generate index.html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ManifestPlugin from 'webpack-manifest-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('production')
@@ -10,19 +12,42 @@ export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: './src/index',
+  entry: {
+    weather_js: [path.resolve(__dirname, 'src/index.js')],
+  },
   target: 'web',
   output: {
-    path: `${__dirname}/dist`, // Note: Physical files are only output by the production build task `npm run build`.
+    path: `${__dirname}/dist`,
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
   },
   devServer: {
     contentBase: './dist',
   },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.less'],
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+      { test: /(\.css)$/, loaders: ['style', 'css'] },
+      {
+        test: /\.less$/,
+        // Use local CSS modules
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules&importLoaders=1!less-loader"),
+        include: path.resolve(__dirname, 'src'),
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    ],
+  },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new ExtractTextPlugin('styles.css'),
+    new ExtractTextPlugin('[name].[contenthash].css'),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.ProvidePlugin({
@@ -41,18 +66,10 @@ export default {
         'API_KEY': `"${process.env.API_KEY}"`,
       },
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
   ],
-  module: {
-    loaders: [
-      { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
-      { test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap") },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-    ],
-  },
   node: {
     console: true,
     fs: 'empty',
