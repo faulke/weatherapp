4ed1f212ebe83890b1facf56cf4dfd6f5f6eac92
faulke <faulkev@gmail.com>
2017-02-15 23:12:56 -0700

    Attempt mobile navbar fix

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,7 @@ import { Navbar, Button } from 'react-bootstrap';
 import styles from './footer.less';
 
 const Footer = ({ onClick }) =>
-  <Navbar inverse fluid fixedBottom>
+  <Navbar inverse fluid fixedBottom className={styles.navbar}>
     <Navbar.Header className={styles.buttons}>
       <Button id="fahr" bsSize="small" onClick={onClick}>&deg;F</Button>
       <Button id="cels" bsSize="small" onClick={onClick}>&deg;C</Button>
