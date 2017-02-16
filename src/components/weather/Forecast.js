3d2a9e66e97adf640b245581313cab1bb811a4ed
faulke <faulkev@gmail.com>
2017-02-15 21:48:15 -0700

    Style forecast for small screens

--- src/components/weather/Forecast.js
+++ src/components/weather/Forecast.js
@@ -7,15 +7,20 @@ const Forecast = ({ data, days, celsius }) => {
   const units = celsius ? 'C' : 'F';
   return (
     <Row className={styles.forecast}>
+      <Col lg={1} md={1} sm={1} />
       {days.map((x, i) => {
         const icon = `wi wi-owm-${data[i].weather[0].id}`;
         let temp = Math.round(data[i].temp.max);
         temp = celsius ? parseInt((temp - 32) * (5 / 9), 10) : temp;
         return (
-          <Col key={i} className={styles.forecastDay}>
-            <h4>{x}</h4>
-            <h2><i className={icon} /></h2>
-            <h3>{temp}&deg;{units}</h3>
+          <Col key={i} className={styles.forecastDayContainer} lg={2} lgOffset={0} md={2} mdOffset={0} sm={2} smOffset={0} xs={8} xsOffset={2}>
+            <div className={styles.forecastDay}>
+              <span className={styles.dayName}>{x}</span>
+              <div className={styles.dayWeather}>
+                <i className={icon} />
+                <span className={styles.temp}>{temp}&deg;{units}</span>
+              </div>
+            </div>
           </Col>
         );
       })}
