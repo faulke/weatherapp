0af8504beb8b71ee06a5b0be7fd593dea0b0d4fe
faulke <faulkev@gmail.com>
2017-03-14 21:05:41 -0600

    Remove buttons from footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Navbar, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import styles from './footer.less';
 
 const Footer = ({ onClick }) =>
-  <Navbar inverse fluid fixedBottom className={styles.navbar}>
-    <Navbar.Header className={styles.buttons}>
-      <Button id="fahr" bsSize="small" onClick={onClick}>&deg;F</Button>
-      <Button id="cels" bsSize="small" onClick={onClick}>&deg;C</Button>
-    </Navbar.Header>
-  </Navbar>;
+  <div className={styles.buttons}>
+    <Button id="fahr" bsSize="small" onClick={onClick}>&deg;F</Button>
+    <Button id="cels" bsSize="small" onClick={onClick}>&deg;C</Button>
+  </div>;
 
 Footer.propTypes = {
   onClick: React.PropTypes.func.isRequired,
