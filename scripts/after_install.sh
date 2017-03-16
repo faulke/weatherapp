e43bf0d1714a153d766d589233f35934b59554d9
faulke <faulkev@gmail.com>
2017-03-15 21:28:32 -0600

    Move app start to own script

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/weatherapp/
+sudo npm install
+sudo npm run build
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/weatherapp/
-sudo npm install
-sudo npm run build
-sudo npm run prod > /dev/null 2> /dev/null < /dev/null &
+sudo babel-node tools/distServer.js > /dev/null 2> /dev/null < /dev/null &
