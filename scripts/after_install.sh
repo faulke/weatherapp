2cb2602a013dcef336ad87a89bad4de8c7084768
faulke <faulkev@gmail.com>
2017-03-15 22:42:39 -0600

    Add forever for app start and stop

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/weatherapp/
 sudo npm install
+sudo npm install forever -g
 sudo npm run build
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/weatherapp/
-sudo babel-node tools/distServer.js > /dev/null 2> /dev/null < /dev/null &
+sudo forever start -c "node -r babel-register" tools/distServer.js > /dev/null 2> /dev/null < /dev/null &
