008723b5d8709f8b402534960f5cfc7f5b1ac28d
faulke <faulkev@gmail.com>
2017-06-11 16:01:33 -0600

    update node env

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const GLOBALS = {
-  'process.env.NODE_ENV': `"${process.env.STAGE}"`
+  'process.env.NODE_ENV': JSON.stringify('production')
 };
 
 export default {
@@ -59,7 +59,7 @@ export default {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': `"${process.env.STAGE}"`,
+        'NODE_ENV': JSON.stringify('production'),
         'USER_NAME': `"${process.env.USER_NAME}"`,
         'PASSWORD': `"${process.env.PASSWORD}"`,
       },
