ec7a07e14f7e147f7efddc60cf11a5b73ed82001
faulke <faulkev@gmail.com>
2017-06-11 18:03:42 -0600

    Revert "Try cors package"
    This reverts commit 541ccc1c3fe0b3275c7d7b855643541805b45b46.

--- tools/distServer.js
+++ tools/distServer.js
@@ -4,34 +4,23 @@ import path from 'path';
 import favicon from 'serve-favicon';
 import api from './routes/api';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(favicon(path.join(__dirname, '../src/favicon.ico')));
 
 app.use(express.static('dist'));
 
-const whitelist = ['https://staging.simpleweather.us', 'https://simpleweather.us']
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
-
 app.use('/api', api);
-
-app.options('*', cors(corsOptions));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
