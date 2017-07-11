90c69e708b263669dd469c597f8d98eb8c981352
faulke <faulkev@gmail.com>
2017-07-10 21:03:08 -0600

    Add forecast endpoint and lambda

---
+++ lambda/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request-promise');
+
+exports.handler = (event, context, callback) => {
+  const uri = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${event.lat}&lon=${event.long}&cnt=5&units=imperial&APPID=${process.env.WEATHER_KEY}`;
+  const options = {
+    uri,
+    json: true,
+  };
+
+  request(options)
+    .then((data) => {
+      const response = {
+        statusCode: 200,
+        data,
+      };
+      callback(null, response);
+    })
+    .catch((err) => {
+      console.log(err);
+      callback(null, 'There was an error');
+    });
+};
